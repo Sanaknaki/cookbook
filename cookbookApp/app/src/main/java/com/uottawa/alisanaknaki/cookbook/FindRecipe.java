23749bb74269109b521756f5e01b2409206c474d
unaizrehmani <unaizrehmani@gmail.com>
2016-11-14 21:22:13 -0500

    Created FindRecipe, linked FindRecipe class to activity, connected findrecipe button with findrecipe activity

---
+++ cookbookApp/app/src/main/java/com/uottawa/alisanaknaki/cookbook/FindRecipe.java
@@ -0,0 +1,14 @@
+package com.uottawa.alisanaknaki.cookbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by unaizrehmani on 2016-11-14.
+ */
+
+public class FindRecipe extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findrecipe);
+    }
+}
