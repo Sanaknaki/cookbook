f6ae6d973f4fac214a2e4a5b62f8a0a4e40d9272
unaizrehmani <unaizrehmani@gmail.com>
2016-11-16 12:17:20 -0500

    Changed layouts for buttons and checkboxes to be more uniform

---
+++ cookbookApp/app/src/main/java/com/uottawa/alisanaknaki/cookbook/Recipe.java
@@ -0,0 +1,15 @@
+package com.uottawa.alisanaknaki.cookbook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by unaizrehmani on 2016-11-16.
+ */
+
+public class Recipe extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_recipes);
+    }
+}
