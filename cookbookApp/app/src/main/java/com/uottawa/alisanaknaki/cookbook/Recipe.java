9d4cbc81c019bd631e127a4758fbf88a49eb3486
Ali Sanaknaki <asana085@uottawa.ca>
2016-11-16 12:52:53 -0500

    Added recipe info page

--- cookbookApp/app/src/main/java/com/uottawa/alisanaknaki/cookbook/Recipe.java
+++ cookbookApp/app/src/main/java/com/uottawa/alisanaknaki/cookbook/Recipe.java
@@ -1,7 +1,10 @@
 package com.uottawa.alisanaknaki.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by unaizrehmani on 2016-11-16.
@@ -11,5 +14,13 @@ public class Recipe extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_recipes);
+
+        Button seeRecipeInfo = (Button) findViewById(R.id.chickenNoodleSoup);
+        seeRecipeInfo.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(Recipe.this,RecipeInfo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ cookbookApp/app/src/main/java/com/uottawa/alisanaknaki/cookbook/RecipeInfo.java
@@ -0,0 +1,15 @@
+package com.uottawa.alisanaknaki.cookbook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AliSanaknaki on 2016-11-16.
+ */
+
+public class RecipeInfo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipeinfo);
+    }
+}
