1c571a8895f72b00436cb2f706d8f3436a15b28d
unaizrehmani <unaizrehmani@gmail.com>
2016-11-28 17:39:50 -0500

    Single recipe screen and functionality is a success.

--- app/src/main/java/com/example/unaizrehmani/finalcookbook/singleRecipeScreen.java
+++ app/src/main/java/com/example/unaizrehmani/finalcookbook/singleRecipeScreen.java
@@ -2,6 +2,8 @@ package com.example.unaizrehmani.finalcookbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -9,7 +11,8 @@ import java.util.ArrayList;
 public class singleRecipeScreen extends AppCompatActivity {
 
     private Recipe recipe;
-    private ArrayList<Ingredient> recipeIngredients;
+    private ArrayList<Ingredient> recipeIngredients = new ArrayList<Ingredient>();
+    private ArrayList<String> recipeStringIngredients = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,11 @@ public class singleRecipeScreen extends AppCompatActivity {
 
         recipe  = (Recipe) getIntent().getExtras().getSerializable("currentRecipe");
         recipeIngredients = recipe.getRecipeIngredients();
+
+        for(int i = 0; i<recipeIngredients.size(); i++){
+            recipeStringIngredients.add(recipeIngredients.get(i).get_IngredientName());
+        }
+
 
         TextView recipeName = (TextView) findViewById(R.id.recipeNamePopUpTextView);
         recipeName.setText(recipe.getRecipeName());
@@ -37,7 +45,9 @@ public class singleRecipeScreen extends AppCompatActivity {
         TextView type = (TextView) findViewById(R.id.getTypeTextView);
         type.setText(recipe.getRecipeType());
 
-
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(singleRecipeScreen.this,android.R.layout.simple_list_item_1,recipeStringIngredients);
+        ListView myList = (ListView) findViewById(R.id.recipePopUpListView);
+        myList.setAdapter(myAdapter);
         //TextView prepTime = (TextView) findViewById(R.id.getPrepTextView);
         //prepTime.setText(recipe.get_prepTime());
     }
