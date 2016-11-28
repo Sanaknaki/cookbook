7d46739fd0716274c969a7ff63d1078ba7df6822
unaizrehmani <unaizrehmani@gmail.com>
2016-11-28 17:32:19 -0500

    Added recipe info stuff

--- app/src/main/java/com/example/unaizrehmani/finalcookbook/singleRecipeScreen.java
+++ app/src/main/java/com/example/unaizrehmani/finalcookbook/singleRecipeScreen.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class singleRecipeScreen extends AppCompatActivity {
 
     private Recipe recipe;
+    private ArrayList<Ingredient> recipeIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,28 @@ public class singleRecipeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_single_recipe_screen);
 
         recipe  = (Recipe) getIntent().getExtras().getSerializable("currentRecipe");
+        recipeIngredients = recipe.getRecipeIngredients();
 
         TextView recipeName = (TextView) findViewById(R.id.recipeNamePopUpTextView);
         recipeName.setText(recipe.getRecipeName());
+
+        TextView recipeCookTime = (TextView) findViewById(R.id.getCookTextView);
+        recipeCookTime.setText(Integer.toString(recipe.get_cookTime()));
+
+        TextView prepTime = (TextView) findViewById(R.id.getPrepTextView);
+        prepTime.setText(Integer.toString(recipe.get_prepTime()));
+
+        TextView calories = (TextView) findViewById(R.id.getCaloriesTextView);
+        calories.setText(Integer.toString(recipe.get_calories()));
+
+        TextView category = (TextView) findViewById(R.id.getCategoryTextView);
+        category.setText(recipe.getRecipeCategory());
+
+        TextView type = (TextView) findViewById(R.id.getTypeTextView);
+        type.setText(recipe.getRecipeType());
+
+
+        //TextView prepTime = (TextView) findViewById(R.id.getPrepTextView);
+        //prepTime.setText(recipe.get_prepTime());
     }
 }
