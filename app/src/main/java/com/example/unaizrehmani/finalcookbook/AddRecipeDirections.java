2c560138820f4b867e3934959ff87ebb05e3907f
unaizrehmani <unaizrehmani@gmail.com>
2016-11-29 02:44:27 -0500

    Finished implementing add recipe

--- app/src/main/java/com/example/unaizrehmani/finalcookbook/AddRecipeDirections.java
+++ app/src/main/java/com/example/unaizrehmani/finalcookbook/AddRecipeDirections.java
@@ -1,7 +1,14 @@
 package com.example.unaizrehmani.finalcookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,6 +22,8 @@ public class AddRecipeDirections extends AppCompatActivity {
     private int prepIntTime;
     private int cookIntTime;
     private int caloriesIntTime;
+
+    private ArrayList<String> directions = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +40,54 @@ public class AddRecipeDirections extends AppCompatActivity {
         caloriesIntTime = (Integer) getIntent().getExtras().getSerializable("caloriesIntTime");
 
 
+        populateListView();
+
+        Button add = (Button) findViewById(R.id.addRecipeDirectionStepButton);
+        Button finish = (Button) findViewById(R.id.createRecipeFinalButton);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userInput = (EditText) findViewById(R.id.addRecipeDirectionEditText);
+                String userStringInput = userInput.getText().toString();
+                userInput.setText("");
+
+                if(userStringInput!=null && !userStringInput.equals("")){
+                    directions.add((directions.size()+1) + ") " + userStringInput);
+                    populateListView();
+                }
+            }
+        });
+
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(directions.size() == 0){
+                    Toast.makeText(getApplicationContext(), "Need to Add at Least One Step.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Recipe recipe = new Recipe(recipeName,chosenCategory,chosenType,chosenAddIngredients,directions,prepIntTime,cookIntTime,caloriesIntTime);
+                    Toast.makeText(getApplicationContext(), ("Added " + recipe + " to Cook Book."), Toast.LENGTH_SHORT).show();
+
+                    cookBook.add_cookBookRecipe(recipe);
+
+                    Intent intent = new Intent(AddRecipeDirections.this,MainScreen.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("cookBook", cookBook);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
     }
+
+    public void populateListView(){
+        ListView myListView = (ListView) findViewById(R.id.addRecipeDirectionListView);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(AddRecipeDirections.this,android.R.layout.simple_list_item_1,directions);
+        myListView.setAdapter(myAdapter);
+    }
+
+
 }
