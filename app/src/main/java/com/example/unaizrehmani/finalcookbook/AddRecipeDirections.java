45d69a0d41777f0d29349913af8378c7f831d8fb
unaizrehmani <unaizrehmani@gmail.com>
2016-11-29 02:11:29 -0500

    Add recipe template

--- app/src/main/java/com/example/unaizrehmani/finalcookbook/AddRecipeDirections.java
+++ app/src/main/java/com/example/unaizrehmani/finalcookbook/AddRecipeDirections.java
@@ -1,13 +1,35 @@
 package com.example.unaizrehmani.finalcookbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class AddRecipeDirections extends AppCompatActivity {
+
+    private MainScreen cookBook;
+    private String recipeName;
+    private String chosenCategory;
+    private String chosenType;
+    private ArrayList<Ingredient> chosenAddIngredients;
+    private int prepIntTime;
+    private int cookIntTime;
+    private int caloriesIntTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe_directions);
+
+        cookBook = (MainScreen) getIntent().getExtras().getSerializable("cookBook");
+        recipeName = (String) getIntent().getExtras().getSerializable("recipeName");
+        chosenCategory = (String)getIntent().getExtras().getSerializable("chosenCategory");
+        chosenType = (String)getIntent().getExtras().getSerializable("chosenType");
+        chosenAddIngredients = (ArrayList<Ingredient>) getIntent().getExtras().getSerializable("chosenAddIngredients");
+        prepIntTime = (Integer) getIntent().getExtras().getSerializable("prepIntTime");
+        cookIntTime = (Integer) getIntent().getExtras().getSerializable("cookIntTime");
+        caloriesIntTime = (Integer) getIntent().getExtras().getSerializable("caloriesIntTime");
+
+
     }
 }
