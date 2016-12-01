f8d5463830504acfc1113f1496680848fb6d0203
unaizrehmani <unaizrehmani@gmail.com>
2016-12-01 05:59:13 -0500

    Fixed some minor errors.

--- app/src/main/java/com/example/unaizrehmani/finalcookbook/EditType.java
+++ app/src/main/java/com/example/unaizrehmani/finalcookbook/EditType.java
@@ -116,7 +116,7 @@ public class EditType extends AppCompatActivity {
                         //Should delete all recipes with this category.
                         for(int i = 0; i<cookBook.get_cookBookRecipes().size(); i++){
                             if(cookBook.get_cookBookRecipes().get(i).getRecipeType().equals(userInputString)){
-                                Toast.makeText(getApplicationContext(),"Deleted Recipe: "+ cookBook.get_cookBookRecipes().get(i).getRecipeName(), Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(getApplicationContext(),"Deleted Recipe: "+ cookBook.get_cookBookRecipes().get(i).getRecipeName(), Toast.LENGTH_SHORT).show();
 
                                 cookBook.get_cookBookRecipes().remove(i);
                                 i = i - 1;
